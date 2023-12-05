08cd6606c1ee7093cf8e74f603b53d4e52bdf9c0
David Lang <david.lang@displayr.com>
2023-12-05 16:35:14 +1100

    Revert "Refactor"
    This reverts commit 28cbc9d00561738f200e65c0a2655af3221f1d60.

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -41,31 +41,6 @@ impl Almanac {
         }
         locations
     }
-
-    fn type1(text: &str) -> Self {
-        let sections: Vec<&str> = text.split("\r\n\r\n").collect();
-        if sections.len() != 8 {
-            panic!("Expected 8 sections, found {}", sections.len());
-        }
-        let seeds = sections[0].split(": ").nth(1).unwrap().split(" ").map(|s| s.parse().unwrap()).collect();
-        let seed_to_soil = sections[1].parse().unwrap();
-        let soil_to_fertilizer = sections[2].parse().unwrap();
-        let fertilizer_to_water = sections[3].parse().unwrap();
-        let water_to_light = sections[4].parse().unwrap();
-        let light_to_temperature = sections[5].parse().unwrap();
-        let temperature_to_humidity = sections[6].parse().unwrap();
-        let humidity_to_location = sections[7].parse().unwrap();
-        Self {
-            seeds,
-            seed_to_soil,
-            soil_to_fertilizer,
-            fertilizer_to_water,
-            water_to_light,
-            light_to_temperature,
-            temperature_to_humidity,
-            humidity_to_location
-        }
-    }
 }
 
 impl FromStr for NumberMap {
@@ -90,14 +65,43 @@ impl FromStr for NumberMap {
     }
 }
 
+impl FromStr for Almanac {
+    type Err = String;
+
+    fn from_str(text: &str) -> Result<Self, Self::Err> {
+        let sections: Vec<&str> = text.split("\r\n\r\n").collect();
+        if sections.len() != 8 {
+            return Err(format!("Expected 8 sections, found {}", sections.len()));
+        }
+        let seeds = sections[0].split(": ").nth(1).unwrap().split(" ").map(|s| s.parse().unwrap()).collect();
+        let seed_to_soil = sections[1].parse().unwrap();
+        let soil_to_fertilizer = sections[2].parse().unwrap();
+        let fertilizer_to_water = sections[3].parse().unwrap();
+        let water_to_light = sections[4].parse().unwrap();
+        let light_to_temperature = sections[5].parse().unwrap();
+        let temperature_to_humidity = sections[6].parse().unwrap();
+        let humidity_to_location = sections[7].parse().unwrap();
+        Ok(Self {
+            seeds,
+            seed_to_soil,
+            soil_to_fertilizer,
+            fertilizer_to_water,
+            water_to_light,
+            light_to_temperature,
+            temperature_to_humidity,
+            humidity_to_location
+        })
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 2 {
         let filename = &args[1];
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
-        let a1: Almanac = Almanac::type1(&text);
-        let locations = a1.locations();
+        let almanac: Almanac = text.parse().unwrap();
+        let locations = almanac.locations();
         println!("Locations: {:?}", locations);
         let lowest_location = locations.iter().min().unwrap();
         println!("Lowest: {}", lowest_location);

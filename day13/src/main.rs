382d6bc3d779269945ca76989fc89abca55509c4
David Lang <david.lang@displayr.com>
2023-12-13 16:37:00 +1100

    part2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -8,7 +8,7 @@ enum Tile {
     Rocks
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 enum Mirror {
     AfterRow(usize),
     AfterColumn(usize)
@@ -40,6 +40,18 @@ impl FromStr for Line {
     }
 }
 
+impl Line {
+    fn diff(&self, other: &Self) -> usize {
+        let mut diff = 0;
+        for i in 0..self.0.len() {
+            if self.0[i] != other.0[i] {
+                diff += 1;
+            }
+        }
+        diff
+    }
+}
+
 impl Tile {
     fn from(c: char) -> Self {
         match c {
@@ -49,6 +61,7 @@ impl Tile {
         }
     }
 }
+
 impl FromStr for Pattern {
     type Err = String;
 
@@ -70,23 +83,38 @@ impl FromStr for Pattern {
 }
 
 impl Pattern {
-    fn mirror(&self) -> Option<Mirror> {
+    fn mirror(&self, allow_smudge: bool, avoid_mirror: Option<&Mirror>) -> Option<Mirror> {
         for r in 0..(self.rows.len() - 1) {
-            if Self::check_mirror(&self.rows, r, r + 1) {
-                return Some(Mirror::AfterRow(r + 1));
+            if Self::check_mirror(&self.rows, r, r + 1, allow_smudge) {
+                let mirror = Mirror::AfterRow(r + 1);
+                if let Some(avoid) = avoid_mirror {
+                    if *avoid == mirror {
+                        continue;
+                    }
+                }
+                return Some(mirror);
             }
         }
         for c in 0..(self.cols.len() - 1) {
-            if Self::check_mirror(&self.cols, c, c + 1) {
-                return Some(Mirror::AfterColumn(c + 1));
+            if Self::check_mirror(&self.cols, c, c + 1, allow_smudge) {
+                let mirror = Mirror::AfterColumn(c + 1);
+                if let Some(avoid) = avoid_mirror {
+                    if *avoid == mirror {
+                        continue;
+                    }
+                }
+                return Some(mirror);
             }
         }
-        return None
+        None
     }
 
-    fn check_mirror(lines: &Vec<Line>, mut lower: usize, mut upper: usize) -> bool {
+    fn check_mirror(lines: &Vec<Line>, mut lower: usize, mut upper: usize, mut allow_smudge: bool) -> bool {
         while upper < lines.len() {
-            if lines[lower] != lines[upper] {
+            let diff = lines[lower].diff(&lines[upper]);
+            if diff == 1 && allow_smudge {
+                allow_smudge = false;
+            } else if diff > 0 {
                 return false;
             }
             if lower == 0 {
@@ -106,13 +134,16 @@ fn main() {
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
         let patterns: Vec<Pattern> = text.split("\r\n\r\n").map(|p| p.parse().unwrap()).collect();
-        let mut sum = 0;
+        let mut sum1 = 0;
+        let mut sum2 = 0;
         for pattern in patterns {
-            let mirror = pattern.mirror().unwrap();
-            //println!("Mirror: {:?}", mirror);
-            sum += mirror.number();
+            let mirror1 = pattern.mirror(false, None).unwrap();
+            let mirror2 = pattern.mirror(true, Some(&mirror1)).unwrap();
+            sum1 += mirror1.number();
+            sum2 += mirror2.number();
         }
-        println!("Total: {}", sum);
+        println!("Part 1: {}", sum1);
+        println!("Part 2: {}", sum2);
     } else {
         println!("Please provide 1 argument: Filename");
     }
